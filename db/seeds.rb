46685b597994fd104a0ae730eda90a41308ddaf3
Thitiporn2045 <142084816+Thitiporn2045@users.noreply.github.com>
2025-06-15 16:54:45 +0700

    Sort tasks by creation date in index and update seed data with new tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,128 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Task.create!([
+  {
+    title: "First Day at TIPCO! Setup Macbooks and everything up!",
+    description: "ตั้งค่า friendslog, basecamp, Tipco'card, door auth และสิ่งจำเป็นที่จำเป็น",
+    completed: true,
+    created_at: Time.parse("2024-02-17")
+  },
+  {
+    title: "NVC Class",
+    description: "non-violence communication class with P'Mho Phi",
+    completed: true,
+    created_at: Time.parse("2024-03-03")
+  },
+  {
+    title: "Scrum Class",
+    description: "Scrum & Agile methodology class with odts",
+    completed: true,
+    created_at: Time.parse("2024-03-04")
+  },
+  {
+    title: "Ruby Class",
+    description: "First time learning Ruby languege",
+    completed: true,
+    created_at: Time.parse("2024-03-10")
+  },
+  {
+    title: "Ruby On Rails Class",
+    description: "Start Ruby On Rails Project with P'Mac",
+    completed: true,
+    created_at: Time.parse("2024-03-11")
+  },
+  {
+    title: "GIT Class",
+    description: "Start GIT 101 with P'Champ",
+    completed: true,
+    created_at: Time.parse("2024-03-17")
+  },
+  {
+    title: "Agile Testing Class",
+    description: "Learning about Agile Testing, Methodology",
+    completed: true,
+    created_at: Time.parse("2024-03-18")
+  },
+  {
+    title: "Playwright Testing",
+    description: "Use knowledge from Agile Testing class with Playwright in this class",
+    completed: true,
+    created_at: Time.parse("2024-03-21")
+  },
+  {
+    title: "Container Fundamental",
+    description: "Container Technology 101",
+    completed: true,
+    created_at: Time.parse("2024-03-24")
+  },
+  {
+    title: "Docker Class",
+    description: "Learning about Docker and how to use it",
+    completed: true,
+    created_at: Time.parse("2024-03-25")
+  },
+  {
+    title: "CI on Gitlabs",
+    description: "Class about Gitlabs CI with P'Dear",
+    completed: true,
+    created_at: Time.parse("2024-03-26")
+  },
+  {
+    title: "Jenkins Class",
+    description: "Learning about Jenkins and how to use it with P'J",
+    completed: true,
+    created_at: Time.parse("2024-03-27")
+  },
+  {
+    title: "Figma Class",
+    description: "Learning about Figma 101 and how to use it",
+    completed: true,
+    created_at: Time.parse("2024-03-31")
+  },
+  {
+    title: "BMA Project",
+    description: "Start working BMA Project",
+    completed: false,
+    created_at: Time.parse("2024-04-01")
+  },
+  {
+    title: "Quest: พิมพ์ดีด 55 คำ ต่อนาที 95%",
+    description: "พัฒนาทักษะการพิมพ์ดีดให้แม่นยำและรวดเร็ว",
+    completed: false,
+    created_at: Time.parse("2024-04-22")
+  },
+  {
+    title: "Quest: Scrum Assessment",
+    description: "ประเมินความเข้าใจ Scrum",
+    completed: false,
+    created_at: Time.parse("2024-04-28")
+  },
+  {
+    title: "Academy Final Test",
+    description: "การสอบประเมินความรู้ปลายคอร์ส",
+    completed: false,
+    created_at: Time.parse("2024-05-20")
+  },
+  {
+    title: "Quest: Git assessment",
+    description: "แบบทดสอบเกี่ยวกับ Git",
+    completed: false,
+    created_at: Time.parse("2024-05-19")
+  },
+  {
+    title: "Quest: Code Kata",
+    description: "ฝึกโจทย์โปรแกรมมิ่งและทักษะการคิดแก้ปัญหา",
+    completed: false,
+    created_at: Time.parse("2024-05-21")
+  },
+  {
+    title: "Quest: ส่ง baseline ออกกำลังกายที่นี่",
+    description: "กิจกรรมวัด baseline เพื่อเริ่มต้นฟิต!",
+    completed: false,
+    created_at: Time.parse("2024-05-30")
+  },
+  {
+    title: "POC Keycloak LINE Login",
+    description: "ทดลองเชื่อมระบบ Auth ด้วย Keycloak + LINE Login",
+    completed: true,
+    created_at: Time.parse("2025-06-13")
+  }
+])
